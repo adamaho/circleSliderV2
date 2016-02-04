2ebd83b430c63925c127d17f90d11199461025d3
Aho <Aho@ahos-macbook-pro.local>
2016-02-04 13:27:24 -0500

    Initial Commit

---
+++ circleSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  circleSlider
+//
+//  Created by Aho on 2016-02-04.
+//  Copyright © 2016 Aho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ circleSliderTests/circleSliderTests.swift
@@ -0,0 +1,36 @@
+//
+//  circleSliderTests.swift
+//  circleSliderTests
+//
+//  Created by Aho on 2016-02-04.
+//  Copyright © 2016 Aho. All rights reserved.
+//
+
+import XCTest
+@testable import circleSlider
+
+class circleSliderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
