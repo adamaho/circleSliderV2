ef2934e431be609ad1cbd8618f9b17533c57f4c4
Aho <Aho@Ahos-MacBook-Pro.local>
2016-02-05 15:14:06 -0500

    second commit

--- circleSlider/ViewController.swift
+++ circleSlider/ViewController.swift
@@ -8,23 +8,41 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var largeCircle: UIImageView!
     @IBOutlet weak var smallCircle: UIImageView!
     @IBOutlet weak var topSlider: UIImageView!
     @IBOutlet weak var bottomSlider: UIImageView!
-    var cx, cy, angle, mag, rad, radb, ox, oy, x, y: CGFloat!
+    @IBOutlet weak var ZeroSeconds: UILabel!
+    @IBOutlet weak var fiveSeconds: UILabel!
+    @IBOutlet weak var tenSeconds: UILabel!
+    @IBOutlet weak var fifteenSeconds: UILabel!
+    @IBOutlet weak var twentySeconds: UILabel!
+    @IBOutlet weak var twentyFiveSeconds: UILabel!
+    @IBOutlet weak var thirtySeconds: UILabel!
+    @IBOutlet weak var thirtyFiveSeconds: UILabel!
+    @IBOutlet weak var fortySeconds: UILabel!
+    @IBOutlet weak var fortyFiveSeconds: UILabel!
+    @IBOutlet weak var fiftySeconds: UILabel!
+    
+    
+    
+    let textColor = UIColor(red: 132/255, green: 132/255, blue: 132/255, alpha: 1)
+
+    
+    var cx, cy, angle, angled, mag, rad, radb, ox, oy, x, y: CGFloat!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Setting the background color of the Canvas
-        let backgroundColor = UIColor(red: 0.4, green: 0.5, blue: 0.7, alpha: 1.0)
+        let backgroundColor = UIColor(red: 177/255, green: 0, blue: 5/255, alpha: 1.0)
         view.backgroundColor = backgroundColor
         
-        
+
         // Setting the color of the Big Circle
         let CircleColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         largeCircle.layer.cornerRadius = 125
@@ -42,7 +60,7 @@ class ViewController: UIViewController {
         
         
         // Setting the color of the topSlider
-        let sliderColor = UIColor(red: 0.6, green: 0.8, blue: 0.9, alpha: 1.0)
+        let sliderColor = UIColor(red: 1, green: 195/255, blue: 70/255, alpha: 1.0)
         topSlider.layer.cornerRadius = 17.5
         topSlider.layer.masksToBounds = true
         topSlider.layer.backgroundColor = sliderColor.CGColor
@@ -52,6 +70,71 @@ class ViewController: UIViewController {
         bottomSlider.layer.masksToBounds = true
         bottomSlider.layer.backgroundColor = sliderColor.CGColor
         
+        // Configuring Label for zero seconds
+        ZeroSeconds.textAlignment = NSTextAlignment.Center
+        ZeroSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        ZeroSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        ZeroSeconds.text = "00"
+        
+        // Configuring Label for five seconds
+        fiveSeconds.textAlignment = NSTextAlignment.Center
+        fiveSeconds.textColor = textColor
+        fiveSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        fiveSeconds.text = "05"
+        
+        // Configuring Label for ten seconds
+        tenSeconds.textAlignment = NSTextAlignment.Center
+        tenSeconds.textColor = textColor
+        tenSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        tenSeconds.text = "10"
+        
+        // Configuring Label for fifteen seconds
+        fifteenSeconds.textAlignment = NSTextAlignment.Center
+        fifteenSeconds.textColor = textColor
+        fifteenSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        fifteenSeconds.text = "15"
+        
+        // Configuring Label for twenty seconds
+        twentySeconds.textAlignment = NSTextAlignment.Center
+        twentySeconds.textColor = textColor
+        twentySeconds.font = UIFont(name: "Azo Sans", size: 12)
+        twentySeconds.text = "20"
+        
+        // Configuring Label for twenty five seconds
+        twentyFiveSeconds.textAlignment = NSTextAlignment.Center
+        twentyFiveSeconds.textColor = textColor
+        twentyFiveSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        twentyFiveSeconds.text = "25"
+        
+        // Configuring Label for thirty seconds
+        thirtySeconds.textAlignment = NSTextAlignment.Center
+        thirtySeconds.textColor = textColor
+        thirtySeconds.font = UIFont(name: "Azo Sans", size: 12)
+        thirtySeconds.text = "30"
+        
+        // Configuring Label for thirty five seconds
+        thirtyFiveSeconds.textAlignment = NSTextAlignment.Center
+        thirtyFiveSeconds.textColor = textColor
+        thirtyFiveSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        thirtyFiveSeconds.text = "35"
+        
+        // Configuring Label for thirty five seconds
+        fortySeconds.textAlignment = NSTextAlignment.Center
+        fortySeconds.textColor = textColor
+        fortySeconds.font = UIFont(name: "Azo Sans", size: 12)
+        fortySeconds.text = "40"
+        
+        // Configuring Label for thirty five seconds
+        fortyFiveSeconds.textAlignment = NSTextAlignment.Center
+        fortyFiveSeconds.textColor = textColor
+        fortyFiveSeconds.font = UIFont(name: "Azo Sans", size: 12)
+        fortyFiveSeconds.text = "45"
+        
+        // Configuring Label for thirty five seconds
+        fiftySeconds.textAlignment = NSTextAlignment.Center
+        fiftySeconds.textColor = textColor
+        fiftySeconds.font = UIFont(name: "Azo Sans", size: 12)
+        fiftySeconds.text = "50"
         
     }
 
@@ -71,9 +154,11 @@ class ViewController: UIViewController {
         oy = 285
         x = cx - ox
         y = oy - cy
-        mag  = sqrt((x*x) + (y*y))
+        mag  = sqrt(pow(x,2) + pow(y,2))
         angle = atan(y/x)
-        rad = 125
+        angled = angle * 180/3.14159
+        print(angled)
+        rad = 123
         radb = 75
         
         // If statement for when x values are greater than 0 wrt the origin of the circle
@@ -81,7 +166,77 @@ class ViewController: UIViewController {
             
             topSlider.center.x = ox + rad*cos(angle)
             topSlider.center.y = oy - rad*sin(angle)
+            
+            
+            // Configuring Angles and text highlighting for 05 seconds
+            if (angled >= 85 && angled <= -85) {
+                
+                ZeroSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                ZeroSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 05 seconds
+            if (angled >= 60 && angled <= 70) {
+                
+                fiveSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                fiveSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 10 seconds
+            if (angled >= 35 && angled <= 45) {
+                
+                tenSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
 
+            } else {
+                
+                tenSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 15 seconds
+            if (angled >= 10 && angled <= 20) {
+                
+                fifteenSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                fifteenSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 20 seconds
+            if (angled >= -28 && angled <= -18) {
+                
+                twentySeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                twentySeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 25 seconds
+            if (angled >= -45 && angled <= -35) {
+                
+                twentyFiveSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                twentyFiveSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 30 seconds
+            if (angled >= -65 && angled <= -55) {
+    
+                thirtySeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                thirtySeconds.textColor = textColor
+            }
             
         }
             
@@ -90,6 +245,46 @@ class ViewController: UIViewController {
             
             topSlider.center.x = ox - rad*cos(angle)
             topSlider.center.y = oy + rad*sin(angle)
+            
+            // Configuring Angles and text highlighting for 35 seconds
+            if (angled >= 53 && angled <= 63) {
+                
+                thirtyFiveSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                thirtyFiveSeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 40 seconds
+            if (angled >= 36 && angled <= 46) {
+                
+                fortySeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                fortySeconds.textColor = textColor
+            }
+            
+            // Configuring Angles and text highlighting for 40 seconds
+            if (angled >= 15 && angled <= 25) {
+                
+                fortyFiveSeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                fortyFiveSeconds.textColor = textColor
+            }
+
+            // Configuring Angles and text highlighting for 40 seconds
+            if (angled >= -23 && angled <= -13) {
+                
+                fiftySeconds.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+                
+            } else {
+                
+                fiftySeconds.textColor = textColor
+            }
 
         }
         
